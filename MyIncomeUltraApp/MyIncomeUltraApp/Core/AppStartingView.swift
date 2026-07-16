ee5d5212884b19b9d935c5bf8339ace3411e86d0
Jeff Menson <jeffrey-14@live.co.uk>
2026-07-16 15:48:08 -0400

    Xcode Project Setup

---
+++ MyIncomeUltraApp/MyIncomeUltraApp/Core/AppStartingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyIncomeUltraApp
+//
+//  Created by Nana Yaw on 7/16/26.
+//
+
+import SwiftUI
+
+struct AppStartingView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  AppStartingView()
+}
--- MyIncomeUltraApp/MyIncomeUltraApp/MyIncomeUltraAppApp.swift
+++ MyIncomeUltraApp/MyIncomeUltraApp/MyIncomeUltraAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyIncomeUltraAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          AppStartingView()
         }
     }
 }
