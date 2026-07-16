3b415d9950b8ea7e26efd06a4148514f3fcca6ce
Jeff Menson <jeffrey-14@live.co.uk>
2026-07-16 15:34:27 -0400

    Initial Xcode Project

---
+++ MyIncomeUltraApp/MyIncomeUltraApp/MyIncomeUltraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIncomeUltraAppApp.swift
+//  MyIncomeUltraApp
+//
+//  Created by Nana Yaw on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MyIncomeUltraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
