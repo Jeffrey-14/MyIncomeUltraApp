70aa5461f0626a7c1292b6dab79c33cf43d07902
Jeff Menson <jeffrey-14@live.co.uk>
2026-07-17 19:42:31 -0400

    Home TabView

---
+++ MyIncomeUltraApp/MyIncomeUltraApp/Core/IncomeView.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeView.swift
+//  MyIncomeUltraApp
+//
+//  Created by Nana Yaw on 7/17/26.
+//
+
+import SwiftUI
+
+struct IncomeView: View {
+  var body: some View {
+    Text("Income View")
+  }
+}
+
+#Preview {
+  IncomeView()
+}
---
+++ MyIncomeUltraApp/MyIncomeUltraApp/Core/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  MyIncomeUltraApp
+//
+//  Created by Nana Yaw on 7/17/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    Text("Settings View")
+  }
+}
+
+#Preview {
+  SettingsView()
+}
+
